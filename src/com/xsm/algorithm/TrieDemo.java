8209d5c9f41bca127bca3601f692290ee0a1a7b2
xushao-fj <xushaomin_it@163.com>
2021-01-07 20:47:57 +0800

    feat:前缀树

--- src/com/xsm/algorithm/TrieDemo.java
+++ src/com/xsm/algorithm/TrieDemo.java
@@ -75,8 +75,40 @@ class JavaTrie {
 
 /** 前缀树版本*/
 class Trie {
+    Trie[] next = new Trie[26];
+    boolean isEnd = false;
 
+    public Trie() {
+    }
 
+    public void insert(String word) {
+        Trie curr = this;
+        for (char c : word.toCharArray()) {
+            if (curr.next[c - 'a'] == null) {
+                curr.next[c - 'a'] = new Trie();
+            }
+            curr = curr.next[c - 'a'];
+        }
+        curr.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        Trie curr = this;
+        for (char c : word.toCharArray()) {
+            if (curr.next[c - 'a'] == null) return false;
+            curr = curr.next[c - 'a'];
+        }
+        return curr.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        Trie curr = this;
+        for (char c : prefix.toCharArray()) {
+            if (curr.next[c - 'a'] == null) return false;
+            curr = curr.next[c - 'a'];
+        }
+        return true;
+    }
 }
 
 
